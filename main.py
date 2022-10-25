1ebb5b7d134a47d2381d5e4f1531eec47d926e94
Darmogra <andridarmogra@gmail.com>
2022-10-25 16:05:29 +0300

    first to meet

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print ('Hello World')
+class Student:
+    amound = 0
+    print("Hi")
+    def __init__(self, name = None):
+        self.name = name
+        Student.amound += 1
+    def __str__(self, name=None):
+        return f'login {self.name}'
+andri = Student()
+dima = Student()
+maris = Student()
+print(dima.name)
+print(andri.name)
+print(dima.amound)
+print(andri.amound)
+print(maris.amound)
+dima = Student(name = 'DIMA GLOBUS')
+print(dima)
